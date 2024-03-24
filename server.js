6ebb0375a0d460c12dcfa898e5ef539eab09095c
Thiago <thiagocaffaro223@gmail.com>
2024-03-23 23:54:02 -0300

    Código melhorado para requisição de dados do mongodb atlas

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+// Substitua <sua-string-de-conexao> pela sua string de conexão do MongoDB Atlas
+mongoose.connect('<sua-string-de-conexao>', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conectado ao MongoDB Atlas'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB Atlas:', err));
+
+// Defina um esquema para os usuários
+const userSchema = new mongoose.Schema({}, { collection: 'users' });
+
+// Crie um modelo para os usuários
+const User = mongoose.model('User', userSchema, 'users');
+
 app.get('/', (req, res) => {
-  res.send('Olá, Mundo!');
+  // Faça uma consulta para buscar todos os usuários
+  User.find({}, function(err, users) {
+    if (err) {
+      console.error(err);
+      res.send('Erro ao buscar usuários');
+    } else {
+      res.send(users);
+    }
+  });
 });
 
 app.listen(port, () => {
