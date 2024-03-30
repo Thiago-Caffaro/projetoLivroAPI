f86b67ca742a13dc08140bad28538feafd0e8ef0
Thiago <thiagocaffaro223@gmail.com>
2024-03-30 18:18:25 -0300

    import alterado para require na url do banco

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors'); 
-import dbUrl from './config';
+const dbUrl = require('./config');
 
 const app = express();
 // Permitir apenas este domínio
