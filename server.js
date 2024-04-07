00a0f7e0b39ea6cd601c8c71085991f762dc33eb
Thiago <thiagocaffaro223@gmail.com>
2024-04-07 16:48:24 -0300

    Adicionei o endpoint addImages para utilizar o multer, para então adicionar várias imagens na collection de imagens

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const cors = require('cors'); 
+const cors = require('cors');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 const dbUrl = require('./config');
 
 const app = express();
@@ -27,7 +29,8 @@ client.connect()
     .then(() => {
         console.log("Conectado com sucesso ao servidor");
         const db = client.db(dbName);
-        const collection = db.collection('talesFromTheLoop');
+        const rpgBaseCollection = db.collection('talesFromTheLoop');
+        const rpgImageCollection = db.collection('talesFromTheLoop');
 
         app.get('/', async (req, res) => {
             try {
@@ -42,7 +45,7 @@ client.connect()
         app.post('/add', async (req, res) => {
             if (req.body) {
                 try {
-                  const result = await collection.insertOne(req.body);
+                  const result = await rpgBaseCollection.insertOne(req.body);
                   res.json(result);
                 } catch (err) {
                   console.log(err.stack);
@@ -52,7 +55,19 @@ client.connect()
                 res.status(400).send('Nenhum corpo de requisição foi enviado');
               }
         });
-
+        app.post('/addImages', upload.array('cardsArray'),async (req, res) => {
+            if (req.body) {
+                try {
+                  const result = await rpgImageCollection.insertOne(req.body);
+                  res.json(result);
+                } catch (err) {
+                  console.log(err.stack);
+                  res.status(500).send(`Ocorreu o erro ao inserir o documento: ${err.stack}`);
+                }
+              } else {
+                res.status(400).send('Nenhum corpo de requisição foi enviado');
+              }
+        });
         app.listen(port, '0.0.0.0', () => {
             console.log(`A API está rodando em http://0.0.0.0:${port}`);
         });
